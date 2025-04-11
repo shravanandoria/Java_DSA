f286b70b472e5f353fd79426a43bf1e466e66041
shravanandoria <36509067+shravanandoria@users.noreply.github.com>
2025-04-11 11:59:36 +0530

    added few more pattern questions

--- src/Patterns/Pattern1.java
+++ src/Patterns/Pattern1.java
@@ -2,7 +2,34 @@ package Patterns;
 
 public class Pattern1 {
     public static void main(String[] args) {
-        pattern1(4);
+        pattern4(5);
+    }
+
+    static void pattern4(int n) {
+        for (int row = 1; row <= n ; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n) {
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= n - i + 1 ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n) {
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= n ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     static void pattern1(int n) {
